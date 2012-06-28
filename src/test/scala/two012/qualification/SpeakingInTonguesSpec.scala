54d7d2bc13ca3f70bd061f29a0d8d988ac5c9009
Jianfeng Tian <jianfeng.tian@gmail.com>
2012-06-28 17:04:46 +0100

    scala solution

--- src/test/scala/two012/qualification/SpeakingInTonguesSpec.scala
+++ src/test/scala/two012/qualification/SpeakingInTonguesSpec.scala
@@ -1,9 +1,12 @@
 package two012.qualification
 
 import org.specs2.mutable._
+import java.io._
+import scala.io.Source._
+import sys.process._
+import java.io.BufferedWriter
 
 class SpeakingInTonguesSpec extends Specification {
-
   "translate 1" in {
     val translator = new SpeakingInTongues
     val translation = translator.translate("ejp mysljylc kd kxveddknmc re jsicpdrysi")
@@ -18,6 +21,16 @@ class SpeakingInTonguesSpec extends Specification {
     val translator = new SpeakingInTongues
     val translation = translator.translate ("de kr kd eoya kw aej tysr re ujdr lkgc jv")
     translation must equalTo("so it is okay if you want to just give up")
-  } 
+  }
+  "full test" in {
+    val translator = new SpeakingInTongues
+    val lines = fromFile(new File("A-small-practice.in")).getLines
+    val casesNumber = lines.next.toInt
+    val oFile = new PrintWriter("A-small-practice.out")
+    (1 to casesNumber).foreach { i =>
+      oFile.println("Case " + i + ": " +  translator.translate(lines.next))
+    }
+    oFile.close
+  }
 }
 
